eb21e28aa9a3997a94305cb3487b41c6e14395c1
nicolasKappa <nicola.kappa8@gmail.com>
2024-03-07 01:55:09 +0400

    Added PasswordHashing for security

--- Website/Connection.php
+++ Website/Connection.php
@@ -1,31 +1,31 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Security/Password Hashing
+    $PasswordHash = password_hash($_POST["Password"], PASSWORD_BCRYPT);
 
-        // Update the database for newly registered user.
-        $Username = $_POST["Username"];
-        $Password = $_POST["Password"];
-        $Email = $_POST["Email"];
 
-        
-        // Database connection parameters
-        $servername = "localhost"; 
-        $db_username = "root"; 
-        $db_password = ""; 
-        $dbname = "recipe_app_database"; 
 
         
-        // Create connection
-        $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+    // Database connection parameters
+    $servername = "localhost"; 
+    $db_username = "root"; 
+    $db_password = ""; 
+    $dbname = "recipe_app_database"; 
 
         
-        // Check connection
+    // Create connection
+    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+        
+    // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }else{
             
-            $stmt = $conn->prepare("Insert into Users(Username,Password,Email)
-                values(?,?,?)");
-            $stmt->bind_param("sss",$Username,$Password,$Email);
+            $stmt = $conn->prepare("INSERT INTO Users(Username,Password,Email) VALUES(?,?,?)");
+            $stmt->bind_param("sss",$_POST["Username"], $PasswordHash, $_POST["Email"]);
+
             
             
         // Behaviour during execution

74401b837674348db50fa58d771f0a7e5ed2c903
nicolasKappa <nicola.kappa8@gmail.com>
2024-03-06 18:23:42 +0400

    Updated

--- Website/Connection.php
+++ Website/Connection.php
@@ -1,27 +1,39 @@
 <?php
         // Update the database for newly registered user.
         $UserID = $_POST["UserID"];
-        $Username = $_POST["Username"];
+        $Username = $_POST["Name"];
         $Password = $_POST["Password"];
         $Email = $_POST["Email"];
 
+        
         // Database connection parameters
         $servername = "localhost"; 
         $db_username = "root"; 
         $db_password = ""; 
         $dbname = "recipe_app_database"; 
 
+        
         // Create connection
         $conn = new mysqli($servername, $db_username, $db_password, $dbname);
 
+        
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }else{
-            $stmt = $conn->prepare("Insert into registration(username,password,email)
+            $stmt = $conn->prepare("Insert into Users(Username,Password,Email)
                 values(?,?,?)");
-            $stmt->bind_param("i,s,s,s",$UserID,$Username,$Password,$Email);
-            echo " Successfully registered";
+            $stmt->bind_param("sss",$Usename,$Password,$Email);
+            
+            
+        // Behaviour during execution
+            if ($stmt->execute()) {
+                echo "Successfully registered";
+            } else {
+                echo "Error: " . $conn->error;
+            }
+            
+            
             $stmt->close();
             $conn->close();
 

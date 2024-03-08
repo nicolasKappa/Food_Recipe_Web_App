b6826d86f6bf79a2fe7c5db1b525f644254f57ed
Christine Parry <114811596+christineparry9@users.noreply.github.com>
2024-03-08 17:23:42 +0100

    added star rating js

--- Website/main.js
+++ Website/main.js
@@ -14,3 +14,32 @@ if (!event.target.matches('.dropbtn')) {
   }
 }
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+  (function(){
+      let sr = document.querySelectorAll('.my-star');
+      let i = 0;
+      //loop through stars
+      while (i < sr.length){
+          //attach click event
+          sr[i].addEventListener('click', function(){
+              //current star
+              let cs = parseInt(this.getAttribute("data-star"));
+              //output current clicked star value
+              document.querySelector('#output').value = cs;
+              /*our first loop to set the class on preceding star elements*/
+              let pre = cs; //set the current star value
+              //loop through and set the active class on preceding stars
+              while(1 <= pre){
+                  //check if the classlist contains the active class, if not, add the class
+                  if(!document.querySelector('.star-'+pre).classList.contains('is-active')){
+                      document.querySelector('.star-'+pre).classList.add('is-active');
+                  }
+                  //decrement our current index
+                  --pre;
+              }//end of first loop
+          })//end of click event
+          i++;
+      }//end of while loop
+  })();//end of function
+})

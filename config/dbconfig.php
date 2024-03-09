549ac0d214c5635e4ac58f458147727c7099ac9f
TheodorHarmse <t.harmse@liverpool.ac.uk>
2024-03-09 12:35:48 +0400

    Teddy: Added dbconfig.php for serice account

---
+++ config/dbconfig.php
@@ -0,0 +1,15 @@
+<?php
+// dbconfig.php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'flavourfinds');
+define('DB_PASSWORD', 'flavourfindsPassword(123)');
+define('DB_DATABASE', 'flavour_finds');
+
+function getConnection() {
+    $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+    if (mysqli_connect_errno()) {
+        die("Failed to connect to MySQL: " . mysqli_connect_error());
+    }
+    return $connection;
+}
+?>

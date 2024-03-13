983cdf91bf1d5d28247103b1577914dc7e449361
Olesia Shtern <olesya0789@gmail.com>
2024-03-13 11:30:49 +0300

    updated comments

--- Website/user_page.php
+++ Website/user_page.php
@@ -74,7 +74,7 @@ $conn->close();
                         <img src="images/logo/logo.png" width="50" height="50" alt="logo">
                     </a>
                 </div>
-// Search bar: user can search recipes here
+<!-- Search bar: user can search recipes here -->
                 <form action="search_results.php" method="get" class="header-form">
                     <input type="search" name="search" placeholder="What do you want to eat today?" id="search-input">
                     <button type="submit">Search</button>
@@ -83,14 +83,14 @@ $conn->close();
                 <nav>
                     <ul class="header-nav">
                         <li class="header-nav-item">
-// Link to redirect to the common page with recipes
+<!-- Link to redirect to the common page with recipes -->
                             <a href="search_results.php">All recipes</a>
                         </li>
                         <li class="header-nav-item dropdown">
                             <a href="javascript:void(0)" class="dropbtn" onclick="myFunction()">
                                 <img src="images/icons/auth-icon.png" alt="User Profile" width="30">
                             </a>
-// Dropdown menu button for logout
+<!-- Dropdown menu button for logout -->
                             <div class="dropdown-content" id="myDropdown" aria label="user search">
                                 <a href="logout.php">Log Out</a>
                             </div>
@@ -105,27 +105,27 @@ $conn->close();
     <main>
         <div class="container">
             <section class="catalog">
-// User's name is displayed ensuring the user is logged in in their profile
+<!-- User's name is displayed ensuring the user is logged in in their profile -->
                 <header class="catalog-header">
                     <div class="catalog-title">
                         <h1>Hi, <?php echo htmlspecialchars($userFullName); ?>!</h1>
                         <h2>Your favorite recipes</h2>
                     </div>
                 </header>
-// Here cards with chosen recipes are displayed
+<!-- Here cards with chosen recipes are displayed -->
                 <div class="catalog-content">
                     <ul class="goods-list">
                         <?php foreach ($favoriteRecipes as $recipe): ?>
                         <li class="goods-item">
                             <div class="product">
-// Photo of the dish
+<!-- Photo of the dish -->
                                 <div class="product-header">
                                     <img src="<?php echo htmlspecialchars($recipe['picture_url']); ?>" alt="<?php echo htmlspecialchars($recipe['title']); ?>">
                                 </div>
 
                                 <div class="product-content">
                                     <a href="recipe.php?id=<?php echo $recipe['recipe_id']; ?>" class="product-title"><?php echo htmlspecialchars($recipe['title']); ?></a>
-// Recipe's rating is displayed here
+<!-- Recipe's rating is displayed here -->
                                     <div class="product-btns">
                                         <div class="product-rating">
                                             <span><?php echo htmlspecialchars(round($recipe['average_rating'])); ?></span>
